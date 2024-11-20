a5ada6ed079391de26688dfb892ca34b4fecec03
Natã de Sousa de Paula <natan345894@gmail.com>
2024-11-20 11:19:45 -0300

    Posições já ocupadas não podem ser ocupadas
    Implementei a lógica de uma posição já ocupada não poder ser ocupada por outro jogador e estou exibindo a mensagem "Vez do Jogador 'X/O'"

--- Jogo_da_Velha/main.py
+++ Jogo_da_Velha/main.py
@@ -4,6 +4,7 @@ import os
 coluna_1 = ["   ","   ","   "]
 coluna_2 = ["   ","   ","   "]
 coluna_3 = ["   ","   ","   "]
+
 linhas = [coluna_1, coluna_2, coluna_3]
 
 num_jogador = 1
@@ -19,28 +20,37 @@ def exibir_tabuleiro_em_branco():
 
 def realizar_movimento(linha_informada, coluna_informada, num_jogador):
     print()
-    for l in range(len(linhas)):
+    movimento_bem_sucedido = True #Não retorno diretamente True ou False, porque preciso que todos os laços sejam percorridos
+    for l in range(len(linhas)):  #para preencher completamente o tabuleiro
         print("          ",end="")
         for c in range(len(coluna_1)):
             if l == linha_informada and c == coluna_informada:
-                if num_jogador == 1: #O SÍMBOLO X REPRESENTA O JOGADOR DE NÚMERO 1
-                    linhas[l][c] = " X "
-                    print(f"[ X ]", end="")
-                else: # O SÍMBOLO O REPRESENTA O JOGADOR DE NÚMERO 2
-                    linhas[l][c] = " O "
-                    print(f"[ O ]", end="")
+                if linhas[l][c] == "   ": #VERIFICANDO SE A POSIÇÃO NÃO FOI OCUPADA
+                    if num_jogador == 1: 
+                        linhas[l][c] = " X "
+                        print(f"[ X ]", end="")
+                    else: 
+                        linhas[l][c] = " O "
+                        print(f"[ O ]", end="")
+                else:                    # CASO A POSIÇÃO JÁ TENHA SIDO OCUPADA, NÃO SERÁ POSSÍVEL REALIZAR O MOVIMENTO
+                    print(f"[{linhas[l][c]}]", end="") 
+                    movimento_bem_sucedido = False
             else:
                 print(f"[{linhas[l][c]}]", end="")
         print()
     print()
+    return movimento_bem_sucedido
 
 os.system('cls')
-print("BEM VINDO AO JOGO DA VELHA")
+print("BEM-VINDO AO JOGO DA VELHA")
 print("OS NÚMEROS POSSÍVEIS PARA LINHA E COLUNA VÃO DE 1 À 3")
 exibir_tabuleiro_em_branco()
 
 while(True):
-    posicao = input("\nDigite a posição para onde deseja se mover (lin,col): ")
+    
+    print(f"\nVEZ DO JOGADOR {"'X'" if num_jogador == 1 else "'O'"}")
+    
+    posicao = input("Digite a posição para onde deseja se mover (lin,col): ")
     if not " " in posicao:
         movimento = posicao.split(",")
     else:
@@ -54,9 +64,12 @@ while(True):
         #Coloquei lin/col - 1 para que o usuário não tenha que ficar pensando que a 1ª linha é a linha 0, 1ª coluna é coluna 0...
     except:
         print("Digite os dados neste formato: (1,1) (2,2) (1,2)")
+        continue
  
     if(linha >= 0 and linha <= 2) and (coluna >= 0 and coluna <= 2): #SE O MOVIMENTO DO USUÁRIO FOR VÁLIDO
-        realizar_movimento(linha, coluna, num_jogador) 
-        num_jogador = 1 if num_jogador == 2 else 2 #TROCANDO A VEZ DO JOGADOR
+        if(realizar_movimento(linha, coluna, num_jogador)): #APENAS SE O MOVIMENTO FOR BEM-SUCEDIDO SERÁ A VEZ DO OUTRO JOGADOR JOGAR
+           num_jogador = 1 if num_jogador == 2 else 2 #TROCANDO A VEZ DO JOGADOR
+        else:
+            print("Movimento inválido! Essa posição já está ocupada.")
     else:
         print("Movimento inválido! Os valores válidos para lin e col vão de 1 a 3.")
